ad8c538f5daa732af24c5379c9e9fa84dbfccd80
Zach Frohardt <zfrohardt@gmail.com>
2021-05-19 13:28:24 -0700

    completed assignment

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,21 +1,23 @@
 function fibonacci(num) {
-  // type your code here
+    if (num < 2) { return num }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+// add your own tests in here
+    console.log("Expecting: 0");
+    console.log("=>", fibonacci(0));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+    console.log("Expecting: 1");
+    console.log("=>", fibonacci(2));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+    console.log("Expecting: 55");
+    console.log("=>", fibonacci(10));
 }
 
 module.exports = fibonacci;
--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,22 +1,39 @@
 def fibonacci(num)
-  # type your code in here
+    # code here
+
+    if num < 0
+        return nil # I hope you dont do this
+    elsif num < 2
+        return num
+    end
+
+    i = 2
+    acc1 = 1
+    acc2 = 1
+    while i < num
+        temp = acc2
+        acc2 = acc2 + acc1
+        acc1 = temp
+        i += 1
+    end
+    acc2
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 0"
-  puts "=>", fibonacci(0)
+    puts "Expecting: 0"
+    puts "=>", fibonacci(0)
 
-  puts
+    puts
 
-  puts "Expecting: 1"
-  puts "=>", fibonacci(2)
+    puts "Expecting: 1"
+    puts "=>", fibonacci(2)
 
-  puts
+    puts
 
-  puts "Expecting: 55"
-  puts "=>", fibonacci(10)
+    puts "Expecting: 55"
+    puts "=>", fibonacci(10)
 
-  # Don't forget to add your own!
+# Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
